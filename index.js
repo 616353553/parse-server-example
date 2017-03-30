688a30e37c7833b31770e1009da5dd39d1e850cf
616353553 <xiaobai1993124>
2017-03-29 21:24:01 -0500

    password policy update

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ var api = new ParseServer({
     // Two optional settings to enforce strong passwords. Either one or both can be specified.
     // If both are specified, both checks must pass to accept the password
     // 1. a RegExp object or a regex string representing the pattern to enforce
-    validatorPattern: /^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.{6,16})/, // enforce password with at least 8 char with at least 1 lower case, 1 upper case and 1 digit
+    validatorPattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,16})/, // enforce password with at least 8 char with at least 1 lower case, 1 upper case and 1 digit
     // 2. a callback function to be invoked to validate the password
     validatorCallback: (password) => { return validatePassword(password) },
     doNotAllowUsername: false, // optional setting to disallow username in passwords
